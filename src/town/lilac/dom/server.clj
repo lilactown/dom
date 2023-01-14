37b277b5daece52650e85e377be00ec184312640
Will Acton <will@willacton.com>
2023-01-13 16:43:15 -0800

    attributes

--- src/town/lilac/dom/server.clj
+++ src/town/lilac/dom/server.clj
@@ -1,4 +1,6 @@
-(ns town.lilac.dom.server)
+(ns town.lilac.dom.server
+  (:refer-clojure :exclude [map meta time])
+  (:require [town.lilac.dom.server.attrs :as attrs]))
 
 (def ^:dynamic *sb* nil)
 
@@ -23,6 +25,7 @@
   (let [^StringBuilder sb *sb*]
     (.append sb "<")
     (.append sb tag)
+    (.append sb (attrs/attrs->str attrs))
     (.append sb ">")
     sb))
 
@@ -31,6 +34,7 @@
   (let [^StringBuilder sb *sb*]
     (.append sb "<")
     (.append sb tag)
+    (.append sb (attrs/attrs->str attrs))
     (.append sb ">")
     sb))
 
@@ -56,6 +60,41 @@
            ~@children
            (close ~tag)))))
 
+
+(declare input textarea option select a abbr address area article aside audio b base bdi
+ bdo big blockquote body br button canvas caption cite code col colgroup data datalist
+ dd del details dfn dialog div dl dt em embed fieldset figcaption figure footer form
+ h1 h2 h3 h4 h5 h6 head header hr html i iframe img ins kbd keygen label legend li link
+ main map mark menu menuitem meta meter nav noscript object ol optgroup output p param
+ picture pre progress q rp rt ruby s samp script section small source span strong style
+ sub summary sup table tbody td tfoot th thead time title tr track u ul var video wbr
+ circle clipPath ellipse g line mask path pattern polyline rect svg text defs
+ linearGradient polygon radialGradient stop tspan)
+
+(def tags
+  '[input textarea option select a abbr address area article aside audio
+    b base bdi bdo big blockquote body br button canvas caption cite code col
+    colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset
+    figcaption figure footer form h1 h2 h3 h4 h5 h6 head header hr html i iframe
+    img ins kbd keygen label legend li link main map mark menu menuitem meta
+    meter nav noscript object ol optgroup output p param picture pre progress q
+    rp rt ruby s samp script section small source span strong style sub summary
+    sup table tbody td tfoot th thead time title tr track u ul var video wbr])
+
+(defn gen-tag
+  [tag]
+  `(defmacro ~tag [& args#]
+     `($ ~(str '~tag) ~@args#)))
+
+(defmacro gen-tags
+  []
+  `(do
+     ~@(for [tag tags]
+         (gen-tag tag))))
+
+(gen-tags)
+
+
 (defn text
   [s]
   (.append ^StringBuilder *sb* s)
@@ -63,9 +102,12 @@
 
 (defn patch
   [f]
-  (binding [*sb* (or *sb* (StringBuilder.))]
-    (f)))
+  (str (binding [*sb* (or *sb* (StringBuilder.))]
+     (f))))
 
 
-(str (patch #($ "div" (text "hi"))))
+(patch #($ "div" (text "hi")))
 ;; => "<div>hi</div>"
+
+
+(patch #(div (text "hi")))
---
+++ test/town/lilac/dom/server_test.clj
@@ -0,0 +1,20 @@
+(ns town.lilac.dom.server-test
+  (:require
+   [clojure.test :as t :refer [deftest is testing]]
+   [town.lilac.dom.server :as d]))
+
+
+(deftest void
+  (is (= "<input>" (d/patch #(d/$ "input")))))
+
+
+(deftest non-void
+  (is (= "<div><button>hi</button></div>"
+         (d/patch #(d/$ "div"
+                        (d/$ "button"
+                             (d/text "hi")))))))
+
+
+(deftest attributes
+  (is (= "<input class=\"foo\">"
+         (d/patch #(d/$ "input" {:class "foo"})))))
